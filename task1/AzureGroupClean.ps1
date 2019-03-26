2c27ab23b62a6aec4a2f0c01e7b813ca0f6cf225
Jakub <sirthaven@gmail.com>
2019-03-26 13:35:14 +0100

    first assigment

---
+++ task1/AzureGroupClean.ps1
@@ -0,0 +1,23 @@
+﻿    param(
+     [Parameter(Mandatory=$True, position=0)]
+     [String] $resourceGroupName, 
+     [ValidateNotNullOrEmpty()]
+     [Parameter(position=1)]
+     [String[]]$ResourceTypes = $("Microsoft.Sql/servers","Microsoft.Web/sites")
+     )
+ 
+
+    foreach ($type in $ResourceTypes) 
+    {
+        Get-AzResource -ResourceGroupName $resourceGroupName -ResourceType $type | ForEach-Object{
+            
+            Write-Output "$($_.Name) found, trying to delete"
+            $result = Remove-AzResource -ResourceGroupName $resourceGroupName -ResourceType $type -ResourceName $_.Name  -Force
+            Write-Output "And this try end with: $($result)"
+        }
+    }
+
+
+
+
+
