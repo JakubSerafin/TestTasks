0d5757124161fa5065c61d4dfc82c83a1de7ae7d
Jakub Serafin <jakub.serafin@heavykinematic.com>
2019-03-30 22:21:43 +0100

    more colors!

--- task2/task2/ModelEntites/EventObject.cs
+++ task2/task2/ModelEntites/EventObject.cs
@@ -1,10 +1,23 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace task2.Models.ModelEntites
 {
+
+    public enum EventType
+    {
+        INCIDENT,//"Awaria/	Interwencja")
+        INFORMATIONAL,//("Informacyjne")
+        COMPLAINT,//("Reklamacja")
+        STATUS,//("Status	sprawy/zgłoszenia")
+        PUBLIC_INFORMATION,//("Wniosek	o	dostęp	do	informacji	publicznej")
+        FREEFORM//("Wolne	wnioski	i	uwagi")
+    }
+
     public class EventObject
     {
         public String Category { get; set; }
@@ -13,5 +26,7 @@ namespace task2.Models.ModelEntites
         public string District { get; set; }
         public string Street2 { get; set; }
         public DateTime createDate { get; set; }
+        [JsonConverter(typeof(StringEnumConverter))]
+        public EventType NotificationType { get; set; }
     }
 }
