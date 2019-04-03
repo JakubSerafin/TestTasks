de3a251581aee07d2692364c1b9f1ab6958b08a5
Jakub Serafin <jakub.serafin@heavykinematic.com>
2019-04-03 23:14:55 +0200

    azure secrets

--- task2/task2/Program.cs
+++ task2/task2/Program.cs
@@ -5,7 +5,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
 using Microsoft.Extensions.Logging;
 
 namespace task2
@@ -19,6 +22,19 @@ namespace task2
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    if (context.HostingEnvironment.IsProduction())
+                    {
+                        var builtConfig = config.Build();
+
+                        var tokenProvider = new AzureServiceTokenProvider();
+                        //Create the Key Vault client
+                        var kvClient = new KeyVaultClient((authority, resource, scope) => tokenProvider.KeyVaultTokenCallback(authority, resource, scope));
+                        //Add Key Vault to configuration pipeline
+                        config.AddAzureKeyVault(builtConfig["KeyVaultName"], kvClient, new DefaultKeyVaultSecretManager());
+                    }
+                })
                 .UseStartup<Startup>();
     }
 }
